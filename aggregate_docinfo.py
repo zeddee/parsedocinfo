61ede6466209bd41d724c93777f9d533fec315fd
zed tan <zed@shootbird.work>
2021-08-13 10:32:38 +0200

    implemented parsing docinfo from file list and returning a dict of DocInfo

--- aggregate_docinfo.py
+++ aggregate_docinfo.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from parsedocinfo.parse_docinfo import parse, DocInfo
-from typing import List
+from typing import Dict, List
 
 INPUTDIR = ""
 
@@ -12,10 +12,26 @@ def get_rst_from_single_dir(dir_path: str) -> List[Path]:
       ]
 
 
-def get_rst_from_dirtree(base_dir: str) -> List[Path]:
-    assert(Path(base_dir).is_dir()), f"{base_dir} is not a valid directory"
-    
-    return list(Path(base_dir).glob("**/*.rst"))
+# def get_rst_from_dirtree(base_dir: str) -> List[Path]:
+#     """
+#     Avoid using. Easier to figure out
+#     dirtree when dealing with a single directory
+#     a time.
+#     """
+#     assert(Path(base_dir).is_dir()), f"{base_dir} is not a valid directory"
+#     
+#     return list(Path(base_dir).glob("**/*.rst"))
+
+def get_doc_info_from_file_list(file_list: List[Path]) -> Dict[str, List[DocInfo]]:
+    out = {}
+    for f in file_list:
+        out[f.stem()] = parse(f)
+
+    return out
 
 if __name__ == "__main__":
-    get_rst_from_dirtree(INPUTDIR)
+    file_list = get_rst_from_single_dir(INPUTDIR)
+
+    aggregated_docinfo_list = get_doc_info_from_file_list(file_list)
+
+    print(aggregated_docinfo_list)

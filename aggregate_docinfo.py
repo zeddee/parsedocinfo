56a222e87b6a39f58c8266f4b93ff59952c7947a
zed tan <zed@shootbird.work>
2021-08-13 10:24:12 +0200

    force Path.glob to return as a list

--- aggregate_docinfo.py
+++ aggregate_docinfo.py
@@ -15,7 +15,7 @@ def get_rst_from_single_dir(dir_path: str) -> List[Path]:
 def get_rst_from_dirtree(base_dir: str) -> List[Path]:
     assert(Path(base_dir).is_dir()), f"{base_dir} is not a valid directory"
     
-    return Path(base_dir).glob("**/*.rst")
+    return list(Path(base_dir).glob("**/*.rst"))
 
 if __name__ == "__main__":
     get_rst_from_dirtree(INPUTDIR)
